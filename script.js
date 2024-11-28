553157fa49b7df800f582b677b2b71c7bdf78024
JULIAN HAZARD <hazardjulian74@gmail.com>
2024-11-28 16:12:45 +0100

    update menu burger

--- script.js
+++ script.js
@@ -25,3 +25,11 @@ const restaurants = [
 },
 ]
 
+const hamMenu = document.querySelector('.burger-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener ('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
