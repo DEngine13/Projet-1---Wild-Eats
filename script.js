e8bbff4badf06c0765cacdb1f585146e1ad0de11
Clément.A <125081819+ITClmt@users.noreply.github.com>
2024-12-02 09:49:23 +0100

    Fixed Description v2

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const restaurants = [
     wildstar: "4",
     Picture: "kohola-STRASBOURG.jpg",
     Description:
-      "Plongez dans l’univers de Koholã avec nos 4 restaurants et venez découvrir notre cuisine d'inspiration hawaïenne avec le poke bowl.",
+      "Plongez dans l’univers de Koholã et venez découvrir notre cuisine d'inspiration hawaïenne avec le poke bowl.",
     GPS: [48.578576911487836, 7.753118633877819],
   },
   {
@@ -75,45 +75,26 @@ const restaurants = [
     wildstar: "4",
     Picture: "aubrasseur-STRASBOURG.jpg",
     Description:
-<<<<<<< HEAD
-      "Le Brasseur, situé à Strasbourg, est une brasserie où les plats délicieux sont accompagnés d'une ambiance charmante. La carte variée propose des salades, des spécialités alsaciennes, des plats de brasserie, des viandes, des welsh, des flammekueche salées et sucrées, des desserts, des glaces et des assiettes de fromages. Des incontournables comme la flammekueche pomme flambée fleur de bière et la glace délice du Brasseur sauront vous séduire. N'hésitez pas à venir découvrir ce lieu où la gastronomie rencontre la convivialité.",
-    GPS: [48.58296036224063, 7.756412216165095],
-  },
-  {
-=======
       "Le Brasseur, situé à Strasbourg, est une brasserie où les plats délicieux sont accompagnés d'une ambiance charmante.",
     GPS: [48.58296036224063, 7.756412216165095]
   }, {
->>>>>>> main
     name: "La Fignette",
     location: "Strasbourg",
     Vegan: "oui",
     wildstar: "5",
     Picture: "LaFignette-STRASBOURG.jpg",
     Description:
-<<<<<<< HEAD
-      "La Fignette, c’est comme une bonne tarte flambée, du feu de bois, des ingrédients locaux et frais, un groupe d’amis autours de la table, des recettes variées et colorées et une occasion à tout moment pour se faire plaisir. La Fignette, c’est un bistrot de quartier, ouvert toute la journée et sur le monde où le voyageur sera toujours accueilli avec la joie du partage et l’habitué comme un ami. La Fignette, c’est un bout d’Alsace forte de sa diversité, de ses valeurs et de sa facilité à parler plusieurs langues. Elle défend ses saveurs et n’a pas peur de les mélanger aux plaisirs d’ailleurs. ",
-    GPS: [48.58257476098657, 7.743042180433159],
-  },
-  {
-=======
-      "La Fignette, c’est comme une bonne tarte flambée, du feu de bois, des ingrédients locaux et frais, un groupe d’amis autours de la table, des recettes variées et colorées et une occasion à tout moment pour se faire plaisir.",
+      "La Fignette, c’est comme une bonne tarte flambée, du feu de bois, et une occasion à tout moment pour se faire plaisir.",
     GPS: [48.58257476098657, 7.743042180433159]
   }, {
->>>>>>> main
     name: "Coco Lobo",
     location: "Strasbourg",
     Vegan: "oui",
     wildstar: "4",
     Picture: "COCOLOBO-STRASBOURG.jpg",
     Description:
-<<<<<<< HEAD
-      "Un Restaurant Tapas mais aussi un Bar de Nuit original et agréable au coeur du quartier de la Petite-France à Strasbourg. Faire la fête sur des rythmes latinos est le maître mot ici, tout en goûtant aux cocktails et aux spécialités culinaires espagnoles !",
-    GPS: [48.579234228037535, 7.739355368789756],
-=======
       "Un Restaurant Tapas mais aussi un Bar de Nuit original et agréable au coeur du quartier de la Petite-France à Strasbourg.",
     GPS: [48.579234228037535, 7.739355368789756]
->>>>>>> main
   },
   {
     name: "Le Hippie Chic Café",
@@ -122,13 +103,8 @@ const restaurants = [
     wildstar: "4",
     Picture: "lehippiechiccafe-MARSEILLE.jpg",
     Description:
-<<<<<<< HEAD
-      "Situé dans le quartier dynamique de la Joliette à Marseille, à deux pas du Panier, notre restaurant offre une expérience culinaire unique. En combinant des ingrédients locaux et notre savoir-faire, nous proposons à nos clients des plats originaux faits maison.",
-    GPS: [43.30439711550707, 5.36575276432541],
-=======
-      "Situé dans le quartier dynamique de la Joliette à Marseille, à deux pas du Panier, notre restaurant offre une expérience culinaire unique.",
+      "Situé dans le quartier dynamique de la Joliette à Marseille, notre restaurant offre une expérience culinaire unique.",
     GPS: [43.30439711550707, 5.36575276432541]
->>>>>>> main
   },
   {
     name: "Le Fantastique",
@@ -137,13 +113,8 @@ const restaurants = [
     wildstar: "5",
     Picture: "lefantastique-MARSEILLE.jpg",
     Description:
-<<<<<<< HEAD
-      "Un restaurant chaleureux et accueillant. Le décor est magique, et la cuisine, faite avec amour, est exquise. Une belle terrasse arrière, un cadre charmant avec une ambiance chaleureuse et des tapas de qualité. Les mets faits maison sont frais, savoureux et goûteux, même s'il y a parfois de l'attente. Un endroit à ne pas manquer pour passer une excellente soirée à Marseille, avec un très bon rapport qualité-prix. Un vrai bijou à découvrir en dehors du tumulte du port.",
-    GPS: [43.28802042739375, 5.389619110199852],
-=======
       "Un restaurant chaleureux et accueillant. Le décor est magique, et la cuisine, faite avec amour, est exquise.",
     GPS: [43.28802042739375, 5.389619110199852]
->>>>>>> main
   },
   {
     name: "Trattoria Partenope",
@@ -152,13 +123,8 @@ const restaurants = [
     wildstar: "4",
     Picture: "trattoria-partenope-MARSEILLE.jpg",
     Description:
-<<<<<<< HEAD
-      "Trattoria Partenope offre une ambiance authentique et conviviale, rappelant les charmantes trattorias italiennes. Les pizzas napolitaines, cuites au four à bois, sont particulièrement appréciées, avec des ingrédients frais et une pâte légère. Les arancini et spaghettis aux supions sont également plébiscités. Le service est chaleureux et attentif, renforçant l'impression d'un vrai voyage culinaire en Italie. Prix très raisonnables.",
-    GPS: [43.29974261866287, 5.366436810382611],
-=======
       "Trattoria Partenope offre une ambiance authentique et conviviale, rappelant les charmantes trattorias italiennes.",
     GPS: [43.29974261866287, 5.366436810382611]
->>>>>>> main
   },
   {
     name: "Full Grill",
@@ -167,13 +133,8 @@ const restaurants = [
     wildstar: "5",
     Picture: "COCOLOBO-STRASBOURG.jpg",
     Description:
-<<<<<<< HEAD
-      "Nouveau restaurant très chaleureux proposant un bel assortiment de viandes grillées, de burgers, salades et tex-mex. Prix très abordable avec une qualité et une belle ambiance au rendez-vous!",
-    GPS: [43.28172155502997, 5.428221252712117],
-=======
       "Nouveau restaurant très chaleureux proposant un bel assortiment de viandes grillées, de burgers, salades et tex-mex.",
     GPS: [43.28172155502997, 5.428221252712117]
->>>>>>> main
   },
   {
     name: "Le Dodo",
@@ -182,7 +143,7 @@ const restaurants = [
     wildstar: "4",
     Picture: "ledodo-MARSEILLE.jpg",
     Description:
-      "Restaurant mauricien proposant des spécialités typiques (rougail, gâteau de manioc...). Venez voyager aux Antilles à travers vos papilles de l'entrée au dessert!",
+      "Restaurant mauricien proposant des spécialités typiques . Venez voyager aux Antilles à travers vos papilles de l'entrée au dessert!",
     GPS: [43.28902099263098, 5.386837598883278],
   },
   {
@@ -192,13 +153,8 @@ const restaurants = [
     wildstar: "5",
     Picture: "daniel&denise-LYON.jpg",
     Description:
-<<<<<<< HEAD
-      "À deux pas de la cathédrale St-Jean, ce bouchon emblématique du Vieux Lyon est tenu par le chef Joseph Viola (Meilleur Ouvrier de France en 2004), déjà connu pour son Daniel et Denise du 3e arrondissement. Au menu de cet opus, une cuisine lyonnaise traditionnelle, qui ravira les amateurs.",
-    GPS: [45.76042846935959, 4.825477771379978],
-=======
-      "À deux pas de la cathédrale St-Jean, ce bouchon emblématique du Vieux Lyon est tenu par le chef Joseph Viola (Meilleur Ouvrier de France en 2004), déjà connu pour son Daniel et Denise du 3e arrondissement.",
+      "À deux pas de la cathédrale St-Jean, ce bouchon emblématique du Vieux Lyon est tenu par le chef Joseph Viola.",
     GPS: [45.76042846935959, 4.825477771379978]
->>>>>>> main
   },
   {
     name: "Les Burgers De Papa",
@@ -207,13 +163,8 @@ const restaurants = [
     wildstar: "4",
     Picture: "lesburgersdepapa-LYON.jpg",
     Description:
-<<<<<<< HEAD
-      "Fondés à Lyon Presqu'île en 2013, les Burgers de Papa ont bien grandi mais restent, dans notre cœur et selon nos goûts, les meilleurs burgers du monde. Papa, il n'a pas changé de ses convictions de nous proposer des burgers gourmands et généreux à base de bons produits locaux. Buns du boulanger, viande 100% française avec des steaks hachés sur place chaque matin, frites maison...",
-    GPS: [45.766672529425584, 4.835641235467729],
-=======
-      "Fondés à Lyon Presqu'île en 2013, les Burgers de Papa ont bien grandi mais restent, dans notre cœur et selon nos goûts, les meilleurs burgers du monde.",
+      "Fondés à Lyon Presqu'île en 2013, les Burgers de Papa ont bien grandi mais restent, les meilleurs burgers du monde.",
     GPS: [45.766672529425584, 4.835641235467729]
->>>>>>> main
   },
   {
     name: "Bartholomé",
@@ -222,13 +173,8 @@ const restaurants = [
     wildstar: "5",
     Picture: "bartholome-lyon.jpg",
     Description:
-<<<<<<< HEAD
-      "Bartholomé, votre restaurant lyonnais spécialisé dans le brunch depuis 2017, vous propose des plats inspirés de l'univers du breakfast Nord Américain, mais aussi des desserts hyper gourmands et maxi sandwichs. Chaque nouvelle carte est élaborée avec soin à partir de produits frais, et toujours avec la volonté de proposer des alternatives sans gluten, avec ou sans lactose ou encore véganes.",
-    GPS: [45.76896033395655, 4.831204396990601],
-=======
-      "Bartholomé, votre restaurant lyonnais spécialisé dans le brunch depuis 2017, vous propose des plats inspirés de l'univers du breakfast Nord Américain, mais aussi des desserts hyper gourmands et maxi sandwichs. ",
+      "Bartholomé, votre restaurant lyonnais spécialisé dans le brunch depuis 2017.",
     GPS: [45.76896033395655, 4.831204396990601]
->>>>>>> main
   },
   {
     name: "Tome",
@@ -237,13 +183,8 @@ const restaurants = [
     wildstar: "4",
     Picture: "tome-LYON.jpg",
     Description:
-<<<<<<< HEAD
-      "La cuisine fusion de ce restaurant offre des repas authentiques. Allez goûter un filet américain délectable dans Tomé si vous n'êtes pas loin. Commandez un parfait savoureux. A ce lieu, les visiteurs peuvent boire un latté délicieux.",
-    GPS: [45.7694251521317, 4.835473723981344],
-=======
-      "La cuisine fusion de ce restaurant offre des repas authentiques. Allez goûter un filet américain délectable dans Tomé si vous n'êtes pas loin.",
+      "La cuisine fusion de ce restaurant offre des repas authentiques.",
     GPS: [45.7694251521317, 4.835473723981344]
->>>>>>> main
   },
   {
     name: "Bistro Abel",
@@ -252,14 +193,9 @@ const restaurants = [
     wildstar: "5",
     Picture: "bistroabel-LYON.jpg",
     Description:
-<<<<<<< HEAD
-      "Le Bistrot d’Abel est le petit frère du Café Comptoir ABEL, à la voute d’Ainay, plus vieux restaurant de Lyon et garant de la tradition des Mères Lyonnaise. Ici, tout est à l’unisson, comme installé depuis des siècles. Une gastronomie bonhomme, mais goûteuse et généreuse, redonne aux palais l’appétence d’une nourriture d’antan. La capitale de la gastronomie trouve ici une nouvelle ambassade du bien manger et du bien recevoir, un nouveau lieu de mœurs culinaires confites dans la tradition ancestrale.",
-    GPS: [45.764193047388254, 4.836881252815976],
-=======
-      "Le Bistrot d’Abel est le petit frère du Café Comptoir ABEL, à la voute d’Ainay, plus vieux restaurant de Lyon et garant de la tradition des Mères Lyonnaise.",
+      "Le Bistrot d’Abel est le plus vieux restaurant de Lyon et garant de la tradition des Mères Lyonnaise.",
     GPS: [45.764193047388254, 4.836881252815976]
 
->>>>>>> main
   },
   {
     name: "La Brigade Du Tigre",
